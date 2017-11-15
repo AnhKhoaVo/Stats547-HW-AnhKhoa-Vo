178fb40ab0f9d643cb571dd900b29810af07e33b
Anh Khoa Vo <a.vo@alumni.ubc.ca>
2017-11-15 11:07:39 -0800

    Minor updates

--- HW7/hw7plot.r
+++ HW7/hw7plot.r
@@ -1,5 +1,5 @@
 library(ggplot2)
 country <- read.csv("hw7.csv")
 
-p <- qplot(fitted, resid, geom = c("point", "smooth"),data=country, main = "Resdiuals vs Fitted Plot")
+p <- qplot(fitted, resid, geom = c("point", "smooth"),data=country, main = "Resdiuals vs Fitted Plot Based on Life Exp")
 ggsave("hw7.png", p)
