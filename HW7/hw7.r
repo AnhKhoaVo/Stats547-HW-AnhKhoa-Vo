a25398b60a090c63d392ce29813d79739059e88d
Anh Khoa Vo <a.vo@alumni.ubc.ca>
2017-11-18 11:08:30 -0800

    minor update

--- HW7/hw7.r
+++ HW7/hw7.r
@@ -1,10 +1,14 @@
 
 Country_dat <- read.delim("gapminder.csv")
 
-
+#linear regression of life expectancy across year and country
 Country.fit <- lm(lifeExp ~ year + factor(country), Country_dat)
+
+#Resdiuals and Fitted Values for the said regression
 resid <- resid(Country.fit)
 fitted <- fitted(Country.fit)
+
+#Combine into a dataset
 country <- cbind(resid, fitted)
 
 
